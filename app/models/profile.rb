f3e21c87838cf4c32687a047276bcb92bf70bf7a
sean-garwood <seansgarwood@gmail.com>
2024-11-19 11:13:21 -0500

    chore: profile nav
    * layout
    * profiles#show/#edit
    * default profile bio callback

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   devise primary_key: :user_id
+
+  before_save { self.bio.nil? ? self.bio = "Welcome to my profile! I have not updated the default bio." : self.bio }
 end
