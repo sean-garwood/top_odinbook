884dd348219f36d72956fb8f4e8ecf9666e5a164
sean-garwood <seansgarwood@gmail.com>
2024-11-21 01:18:02 -0500

    chore: follow rq nav
    TODO: fix accept/reject flow

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -5,6 +5,7 @@ class FollowRequest < ApplicationRecord
   enum :status, { pending: 0, accepted: 1, rejected: 2, blocked: 3 }
 
   scope :accepted, -> { where(status: :accepted) }
+  scope :received, ->(user) { where(status: :pending, recipient_id: user.id) }
 
   validate :not_following_self
   validates :recipient_id, uniqueness: { scope: :user_id, message: "already requested" }
