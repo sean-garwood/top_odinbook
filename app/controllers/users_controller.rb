e0d089226872d2deb569032446e943e21b1534d1
sean-garwood <seansgarwood@gmail.com>
2024-11-26 14:49:43 -0500

    chore: fiddling with layouts
    TODO: make the footer stick to the bottom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all.includes(:profile)
+    @users = User.all.includes(:profile).where.not(id: current_user.id)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,18 +1,29 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should get discover" do
     create_and_sign_in_user
     get discover_path
     assert_response :success
   end
 
-  test "index should list all users" do
-    @count = User.count
+  test "discover should list all users except self" do
+    @count = User.count - 1
     sign_in users(:one)
     get discover_path
-    # count number of users-container divs
-    # assert that the count is equal to the number of users in the database
     assert_select ".profile-banner", @count
+  end
+
+  test "discover should list buttons to follow users" do
+    sign_in users(:one)
+    get discover_path
+    assert_select "form[action=?]", follow_requests_path
+  end
+
+  test "list of users should display username, not email" do
+    sign_in users(:one)
+    get discover_path
+    assert_select ".profile-banner", text: users(:two).name
+    assert_select ".profile-banner", text: users(:two).email, count: 0
   end
 end
